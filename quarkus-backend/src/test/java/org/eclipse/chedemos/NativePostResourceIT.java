a64396add4f8bad8e9a18da6de74b99869775ed6
user1 <sutan@redhat.com>
2019-11-13 16:04:58 +0000

    enable s2i for jvm app

--- quarkus-backend/src/test/java/org/eclipse/chedemos/NativePostResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.chedemos;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativePostResourceIT extends PostResourceTest {
-
-    // Execute the same tests but in native mode.
-}
